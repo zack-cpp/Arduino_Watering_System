95230c56a4e4cc0f486c8cb4d3e246eb92a4cdf6
zack-cpp <neojoyko@gmail.com>
2020-11-11 22:13:26 +0700

    Add feature: next watering and show it from LCD

--- RTC Watering System/RTC_Watering_System.ino
+++ RTC Watering System/RTC_Watering_System.ino
@@ -1,9 +1,9 @@
 /*
- * EEPROM Mapping
- * address 0 --> 2                      : realtime clock (hh:mm:ss)
- * address 3                            : amount of pattern
- * address 4 =< (i*5) - 1 --> (i*5) + 3 : watering time (schedule number, duration in minutes, duration in seconds, hour, minute)
- */
+   EEPROM Mapping
+   address 0 --> 2                      : realtime clock (hh:mm:ss)
+   address 3                            : amount of pattern
+   address 4 =< (i*5) - 1 --> (i*5) + 3 : watering time (schedule number, duration in minutes, duration in seconds, hour, minute)
+*/
 #include <EEPROM.h>
 #include <Keypad.h>
 #include <LCD.h>
@@ -17,26 +17,22 @@
 const byte ROWS = 4;
 const byte COLS = 4;
 
-byte rowPins[ROWS] = {9,8,7,6};
-byte colPins[COLS] = {5,4,3,2};
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
 
-bool state[340];
+bool state[204];
 bool stateDurasi = false;
-bool watering = false;
-bool wateringDone = false;
 bool buttonPressed = false;
-bool next = false;
 
 String input = "";
-String jam,menit;
+String jam, menit;
 
-int iJam,iMenit,counter = 0, wateringCounter = 0;
+int iJam, iMenit;
 
 unsigned int durasiInSecond = 0;
 unsigned int currentWatering = 0;
 unsigned int tmpSecond = 0;
-unsigned int tick = 0;
-unsigned int nextID = 0;
+unsigned int nextID = 1;
 
 char KeysID[] = {'1',  '2', '3', 'A', '4', '5', '6', 'B', '7', '8', '9', 'C', '*', '0', '#', 'D'};
 char keys[ROWS][COLS] = {
@@ -62,22 +58,22 @@ void setDurasi(int durasi);
 void setup() {
   Serial.begin(115200);
   //rtc setup
-  if(!rtc.begin()){
+  if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     abort();
-  }else{
+  } else {
     Serial.println("RTC Started");
   }
-  if(!rtc.isrunning()){
+  if (!rtc.isrunning()) {
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
   //
-  pinMode(pump,OUTPUT);
-  digitalWrite(pump,HIGH);
-  lcd.begin(16,2);
+  pinMode(pump, OUTPUT);
+  digitalWrite(pump, HIGH);
+  lcd.begin(16, 2);
   lcd.backlight();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   reset();
 }
 
@@ -87,76 +83,78 @@ void loop() {
   EEPROM.write(0, now.hour());
   EEPROM.write(1, now.minute());
   EEPROM.write(2, now.second());
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   // show jadwal
   lcd.print("Jadwal: ");
-  if(EEPROM.read((nextID*5) + 2) < 10){
+  if (EEPROM.read((nextID * 5) + 2) < 10) {
     lcd.print("0");
   }
-  lcd.print(EEPROM.read((nextID*5) + 2));
+  lcd.print(EEPROM.read((nextID * 5) + 2));
   lcd.print(":");
-  if(EEPROM.read((nextID*5) + 3) < 10){
+  if (EEPROM.read((nextID * 5) + 3) < 10) {
     lcd.print("0");
   }
-  lcd.print(EEPROM.read((nextID*5) + 3));
+  lcd.print(EEPROM.read((nextID * 5) + 3));
   //
-  lcd.setCursor(0,1);
-  if(EEPROM.read(0) < 10){
+  //show realtime clock
+  lcd.setCursor(0, 1);
+  if (EEPROM.read(0) < 10) {
     lcd.print("0");
   }
   lcd.print(EEPROM.read(0));
   lcd.print(":");
-  if(EEPROM.read(1) < 10){
+  if (EEPROM.read(1) < 10) {
     lcd.print("0");
   }
   lcd.print(EEPROM.read(1));
   lcd.print(":");
-  if(EEPROM.read(2) < 10){
+  if (EEPROM.read(2) < 10) {
     lcd.print("0");
   }
   lcd.print(EEPROM.read(2));
+  //
   char key = keypad.getKey();
-  if(key){
-    if(key == KeysID[12]){
+  if (key) {
+    if (key == KeysID[12]) {
       lcd.clear();
-      lcd.setCursor(0,0);
+      lcd.setCursor(0, 0);
       lcd.print("Watering Time!!!");
       buttonPressed = true;
       startWatering(30);
-    }else if(key == KeysID[14]){
+    } else if (key == KeysID[14]) {
       addSchedule(EEPROM.read(AMOUNT_PATTERN) + 1);
-    }else if(key == KeysID[3]){
-      for(unsigned int i = 0; i < EEPROM.length(); i++){
+    } else if (key == KeysID[3]) {
+      for (unsigned int i = 0; i < EEPROM.length(); i++) {
         EEPROM.write(i, 0);
       }
       reset(); // reset EEPROM
       lcd.clear();
-      lcd.setCursor(0,0);
-    }else if(key == KeysID[7]){
-      if(!rtc.begin()){
+      lcd.setCursor(0, 0);
+    } else if (key == KeysID[7]) {
+      if (!rtc.begin()) {
         Serial.println("Couldn't find RTC");
         Serial.flush();
         abort();
       }
       rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // adjust clock
-    }else if(key == KeysID[11]){
+    } else if (key == KeysID[11]) {
       addSchedule(EEPROM.read(AMOUNT_PATTERN) + 1);
       setDurasi(EEPROM.read(AMOUNT_PATTERN) + 1);
     }
   }
 }
 
-void reset(){
-  if(EEPROM.read(AMOUNT_PATTERN) == 0){
+void reset() {
+  if (EEPROM.read(AMOUNT_PATTERN) == 0) {
     addSchedule(1);
     setDurasi(1);
-    for(unsigned short int i = 0; i < EEPROM.length(); i++){
+    for (unsigned short int i = 0; i < EEPROM.length(); i++) {
       state[i] = false;
     }
   }
 }
 
-void addSchedule(int schedule){
+void addSchedule(int schedule) {
   lcd.clear();
   lcd.print("Add Schedule");
   input = "";
@@ -164,7 +162,7 @@ void addSchedule(int schedule){
   getClock(schedule);
 }
 
-void setDurasi(int schedule){
+void setDurasi(int schedule) {
   lcd.clear();
   lcd.print("Set Durasi");
   input = "";
@@ -172,41 +170,41 @@ void setDurasi(int schedule){
   getClock(schedule);
 }
 
-void setWateringTime(int schedule){
+void setWateringTime(int schedule) {
   lcd.clear();
   lcd.print("Watering Time");
   input = "";
   getClock(schedule);
 }
 
-void getClock(int schedule){
-  lcd.setCursor(2,2);
+void getClock(int schedule) {
+  lcd.setCursor(2, 2);
   lcd.print(":");
   unsigned int jml = 0;
-  while(true){
+  while (true) {
     char key = keypad.getKey();
-    if(key){
-      if(key != KeysID[3] && key != KeysID[7] && key != KeysID[11] && key != KeysID[12] && key != KeysID[14] && key != KeysID[15]){
-        if(jml == 2){
+    if (key) {
+      if (key != KeysID[3] && key != KeysID[7] && key != KeysID[11] && key != KeysID[12] && key != KeysID[14] && key != KeysID[15]) {
+        if (jml == 2) {
           input += ":";
           jml++;
         }
         input += key;
         jml++;
-        for(unsigned int x = 0; x < input.length(); x++){
-          lcd.setCursor(x,1);
+        for (unsigned int x = 0; x < input.length(); x++) {
+          lcd.setCursor(x, 1);
           lcd.print(input[x]);
         }
-        if(jml == 5){
-            Serial.println("break");
-            break;
+        if (jml == 5) {
+          Serial.println("break");
+          break;
         }
       }
     }
   }
   decodeClock(schedule);
 }
-void decodeClock(int schedule){
+void decodeClock(int schedule) {
   jam = "";
   menit = "";
   jam += input[0];
@@ -218,87 +216,75 @@ void decodeClock(int schedule){
   Serial.print(iJam);
   Serial.print(":");
   Serial.println(iMenit);
-  if(iJam > 23 || iMenit > 59){
+  if (iJam > 23 || iMenit > 59) {
     lcd.clear();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     Serial.print("INPUT SALAH");
-   addSchedule(schedule);
+    addSchedule(schedule);
   }
-  if(stateDurasi){
+  if (stateDurasi) {
     EEPROM.write(AMOUNT_PATTERN, EEPROM.read(AMOUNT_PATTERN) + 1);
-    EEPROM.write(schedule*5 - 1, schedule);
-    EEPROM.write(schedule*5, iJam);
-    EEPROM.write(schedule*5 + 1, iMenit);
+    EEPROM.write(schedule * 5 - 1, schedule);
+    EEPROM.write(schedule * 5, iJam);
+    EEPROM.write(schedule * 5 + 1, iMenit);
     stateDurasi = false;
-  }else{
-    EEPROM.write(schedule*5 + 2, iJam);
-    EEPROM.write(schedule*5 + 3, iMenit);
+  } else {
+    EEPROM.write(schedule * 5 + 2, iJam);
+    EEPROM.write(schedule * 5 + 3, iMenit);
   }
   lcd.clear();
 }
-void checkTime(){ //gurung bener
+void checkTime() {
   DateTime now = rtc.now();
   int checkSchedule = EEPROM.read(AMOUNT_PATTERN);
-  if(checkSchedule != 0){
-    for(int schedule = 0; schedule < checkSchedule; schedule++){
+  if (checkSchedule != 0) {
+    for (int schedule = 0; schedule < checkSchedule; schedule++) {
+      unsigned int ANDstate = 0;
       int checkHour = EEPROM.read(((schedule + 1) * 5) + 2);
-      int checkMinute = EEPROM.read(((schedule + 1) *5) + 3);
-      if(now.hour() < checkHour){
-        if(now.minute() < checkMinute){
-          
-        }else{
-          if(next == false){
-            next = true;
-            nextID++;
-          }
-        }
-      }else if(now.hour() == checkHour){
-        if(now.minute() < checkMinute){
-          if(next == false){
-            next = true;
-            nextID++;
-          }
-        }
-      }
+      int checkMinute = EEPROM.read(((schedule + 1) * 5) + 3);
       int checkDurasiMinute = EEPROM.read((schedule + 1) * 5);
       int checkDurasiSecond = EEPROM.read(((schedule + 1) * 5) + 1);
       durasiInSecond = (checkDurasiMinute * 60) + checkDurasiSecond;
-      if(now.hour() == checkHour && now.minute() == checkMinute && wateringDone == false || buttonPressed == true){
-        if(state[schedule] == false){
+      if (now.hour() == checkHour && now.minute() == checkMinute || buttonPressed == true) {
+        if (state[schedule] == false) { // in case duration is lower then 60 seconds, prevent watering more than once in a minute.
           state[schedule] = true;
-          next = false;
           startWatering(durasiInSecond);
-          wateringDone = true;
-          if(nextID == checkSchedule){
-            nextID = 0;
+          nextID++;
+          if (nextID == checkSchedule + 1) {
+            nextID = 1;
           }
         }
-      }else if(now.hour() != checkHour){
-        if(now.minute() != checkMinute){
-          wateringDone = false;
+      }
+      //state reset if all watering is done in a day
+      for (int i = 0; i < checkSchedule; i++) {
+        if (state[i] == true) {
+          ANDstate++;
+        }
+      }
+      if (ANDstate == checkSchedule) {
+        for (int i = 0; i < checkSchedule; i++) {
+          state[i] = false;
         }
       }
     }
   }
 }
 
-void startWatering(int durasi){
+void startWatering(int durasi) {
   DateTime now = rtc.now();
-  watering = true;
-  digitalWrite(pump,LOW);    //ACTIVE LOW
+  digitalWrite(pump, LOW);   //ACTIVE LOW
   lcd.clear();
-  tmpSecond = (3600*now.hour()) + (60*now.minute()) + now.second() + durasi;
-  while(true){
+  tmpSecond = (3600 * now.hour()) + (60 * now.minute()) + now.second() + durasi;
+  while (true) {
     DateTime now = rtc.now();
     delay(10);
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("Watering Time!");
-    if(tmpSecond == (3600*now.hour()) + (60*now.minute()) + now.second()){
-      watering = false;
+    if (tmpSecond == (3600 * now.hour()) + (60 * now.minute()) + now.second()) {
       durasiInSecond = 0;
       tmpSecond = 0;
       digitalWrite(pump, HIGH);
-      if(buttonPressed){
+      if (buttonPressed) {
         buttonPressed = !buttonPressed;
       }
       lcd.clear();
